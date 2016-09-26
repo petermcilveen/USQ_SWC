cccdc3f49a10300a6f12b53f2a1c9a768eb53600
mcilveen <peter.mcilveen@usq.edu.au>
2016-09-26 16:05:12 +1000

    first commit

---
+++ autosplit.sh
@@ -0,0 +1,9 @@
+#create continent subdirectry
+mkdir "$2"
+## split data from "$1"filename.ext using pattern match variable
+## "$2" then save output in current directiory to match variable string name "$2"
+###using matching variable string "$2".csv as filename
+
+#awk '/"$2"/{print $0}' "$1" > ./"$2"/"$2".csv
+grep $2 $1 > $2/$2.csv
+
